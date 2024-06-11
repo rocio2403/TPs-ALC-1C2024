a310e3bd6e722e44ab13f6948e08f2fd77e1df99
Abril Magali Ibarra <102565432+IMAbril@users.noreply.github.com>
2024-06-10 21:03:04 -0300

    Add files via upload

--- pca.py
+++ pca.py
@@ -10,9 +10,6 @@ Tema:  Contenido nutricional de la Canasta Básica
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import os
-from matplotlib.colors import ListedColormap
-os.environ["OMP_NUM_THREADS"] = "1"
 from sklearn.cluster import KMeans
 import seaborn as sns
 
@@ -67,11 +64,9 @@ def componentes_principalesSVD(X, k):
     """
     Dada una matriz X, computa su factorización SVD y retorna la proyección
     sobre el subespacio generado por los primeros k vectores singulares izquierdos
-    de X.     
+    de X y las matrices truncadas de la SVD U_k y Vt_k.
     """
     # Centramos los vectores columna (características) según sus promedios
-    # media = np.mean(X, axis=0, keepdims=True)  
-    # X = X - media  
     n, d = X.shape
     m=np.mean(X, axis=1)
 
@@ -89,9 +84,11 @@ def componentes_principalesSVD(X, k):
     # Calculamos los primeros k vectores singulares izquierdos de X
     U_k = np.dot(X, Vt_k)
     U_k /= np.linalg.norm(U_k, axis=0)    
+    # Matriz de reflexión en el eje de la primer componente
+    R1 = np.array([[1, 0], [0, -1]])
     # Proyectamos los datos en el subespacio generado por los primeros k vectores singulares izquierdos
-    X_proyectado = U_k @ U_k.T @ X
-    return X_proyectado
+    X_proyectado = X @ Vt_k @ R1 
+    return X_proyectado, U_k, Vt_k
 
 
 def graficarProyeccion(X_proyectado, alimentos, n_clusters=4):
@@ -121,6 +118,104 @@ def graficarProyeccion(X_proyectado, alimentos, n_clusters=4):
     
     return df_clusters
 
+def graficos_cluster():
+            
+    # Para el grupo 1
+    grupo1 = df_clusters[df_clusters['Número de Cluster'] == 0]['Alimento'].tolist()
+    grupo1 = tabla_nutricional[tabla_nutricional['Alimento'].isin(grupo1)]
+    grupo1 = grupo1.merge(df_clusters, on='Alimento', how='left')
+        
+    # Para el grupo 2
+    grupo2 = df_clusters[df_clusters['Número de Cluster'] == 1]['Alimento'].tolist()
+    grupo2 = tabla_nutricional[tabla_nutricional['Alimento'].isin(grupo2)]
+    grupo2 = grupo2.merge(df_clusters, on='Alimento', how='left')
+        
+    # Para el grupo 3
+    grupo3 = df_clusters[df_clusters['Número de Cluster'] == 2]['Alimento'].tolist()
+    grupo3 = tabla_nutricional[tabla_nutricional['Alimento'].isin(grupo3)]
+    grupo3 = grupo3.merge(df_clusters, on='Alimento', how='left')
+        
+    # Para el grupo 4
+    grupo4 = df_clusters[df_clusters['Número de Cluster'] == 3]['Alimento'].tolist()
+    grupo4 = tabla_nutricional[tabla_nutricional['Alimento'].isin(grupo4)]
+    grupo4 = grupo4.merge(df_clusters, on='Alimento', how='left')
+        
+        
+    # Lista de macronutrientes
+    macronutrientes = ['HC (gr)', 'Proteinas (gr)', 'Azucares Libres (gr)', 'Grasas (gr)', 'Fibra (gr)']
+        
+    df_combined = pd.concat([grupo1, grupo2, grupo3, grupo4])
+        
+    promedios_grupos = df_combined.groupby('Número de Cluster')[macronutrientes].mean().reset_index()
+        
+    sns.set(style="darkgrid")
+        
+    fig, axes = plt.subplots(2, 3, figsize=(18, 10), sharey=False)
+        
+    # Aplanar los ejes para iterar fácilmente
+    axes = axes.flatten()
+        
+    for idx, macronutriente in enumerate(macronutrientes):
+        sns.barplot(ax=axes[idx], x='Número de Cluster', y=macronutriente, data=promedios_grupos, palette='viridis')
+            
+        axes[idx].set_title(f'Promedio de {macronutriente} por Cluster')
+        axes[idx].set_xlabel('Número de Cluster')
+        axes[idx].set_ylabel(macronutriente)
+        
+    # Eliminar el último subgráfico si hay menos de 6
+    if len(macronutrientes) < len(axes):
+        fig.delaxes(axes[-1])
+        
+    # Ajustar el layout para evitar solapamientos
+    plt.tight_layout()
+        
+        
+    plt.show()
+
+def palabras_contenidas(nombre1, nombre2):
+    """
+    Verifica si todas las palabras de una cadena (excepto casos excepcionales) están contenidas en otra cadena
+    o viceversa, retorna true si se cumple cualquiera de las dos condiciones,
+    de lo contrario, retorna false.
+    
+    """
+    casos_excepcionales =['dulce de leche', 'harina maiz', 'leche entera en polvo', 'paleta cocida', 'tomate envasado', 'vina re']
+    if nombre1 not in casos_excepcionales:
+        palabras1 = nombre1.split()
+        for palabra in palabras1:
+            if palabra != 'en' and palabra != 'de' and palabra in nombre2:
+                return True
+    return 
+
+def filtrarAlimentos(Y, X):
+    """
+    Recibe 2 dataframes de información de alimentos, compara sus primeras columnas
+    y devuelve los datos de X filtrados según sus alimentos que aparecen en Y. 
+    """
+    # Convertimos los nombres a minúsculas para una comparación insensible a mayúsculas/minúsculas
+    X.iloc[:,0] = X.iloc[:,0].str.lower()
+    Y.iloc[:,0] = Y.iloc[:,0].str.lower()
+    
+    # Guardamos los alimentos a filtrar 
+    alimentos_Y = Y.iloc[:,0].tolist()
+
+    #Filtramos
+    filtro = X.iloc[:, 0].apply(lambda x: any(palabras_contenidas(x, alimento) for alimento in alimentos_Y))
+
+    return X[filtro]
+
+def proyectar(Y, proyector):
+    """
+    Proyecta la matriz Y en el subespacio generado por Vt_k obtenido de la matriz X.
+    """
+    # Centramos Y 
+    n, d = Y.shape
+    m=np.mean(Y, axis=1)
+    Y = Y - np.tile(m.reshape((len(m), 1)), (1, d))
+
+    # Proyectamos en el subespacio de las primeras k componentes principales
+    Y_proyectado = Y @ proyector
+    return Y_proyectado    
 # =============================================================================
 # SCRIPT (poner en celdas)
 # =============================================================================
@@ -135,65 +230,23 @@ consumidores_libres = pd.read_csv(carpeta + 'consumidores_libres.csv', delimiter
 tabla_nutricional = normalizar_tabla_nutricional(tabla_nutricional)
 alimentos = tabla_nutricional['Alimento'].values 
 
-# Reducimos la dimensionalidad de los datos mediante PCA
+# Reducimos la dimensionalidad de los datos (X) mediante PCA
 matriz_nutricional = tabla_nutricional.iloc[:, 1:].values
-X_proyectado = componentes_principalesSVD(matriz_nutricional, 2)
+X_proyectado, U_2, Vt_2= componentes_principalesSVD(matriz_nutricional, 2)
 
 # Graficamos la proyección con los clusters y obtenemos el DataFrame de clusters
 df_clusters = graficarProyeccion(X_proyectado, alimentos)
-
-def graficos_cluster():
-            
-        # Para el grupo 1
-        grupo1 = df_clusters[df_clusters['Número de Cluster'] == 0]['Alimento'].tolist()
-        grupo1 = tabla_nutricional[tabla_nutricional['Alimento'].isin(grupo1)]
-        grupo1 = grupo1.merge(df_clusters, on='Alimento', how='left')
-        
-        # Para el grupo 2
-        grupo2 = df_clusters[df_clusters['Número de Cluster'] == 1]['Alimento'].tolist()
-        grupo2 = tabla_nutricional[tabla_nutricional['Alimento'].isin(grupo2)]
-        grupo2 = grupo2.merge(df_clusters, on='Alimento', how='left')
-        
-        # Para el grupo 3
-        grupo3 = df_clusters[df_clusters['Número de Cluster'] == 2]['Alimento'].tolist()
-        grupo3 = tabla_nutricional[tabla_nutricional['Alimento'].isin(grupo3)]
-        grupo3 = grupo3.merge(df_clusters, on='Alimento', how='left')
-        
-        # Para el grupo 4
-        grupo4 = df_clusters[df_clusters['Número de Cluster'] == 3]['Alimento'].tolist()
-        grupo4 = tabla_nutricional[tabla_nutricional['Alimento'].isin(grupo4)]
-        grupo4 = grupo4.merge(df_clusters, on='Alimento', how='left')
-        
-        
-        # Lista de macronutrientes
-        macronutrientes = ['HC (gr)', 'Proteinas (gr)', 'Azucares Libres (gr)', 'Grasas (gr)', 'Fibra (gr)']
-        
-        df_combined = pd.concat([grupo1, grupo2, grupo3, grupo4])
-        
-        promedios_grupos = df_combined.groupby('Número de Cluster')[macronutrientes].mean().reset_index()
-        
-        sns.set(style="darkgrid")
-        
-        fig, axes = plt.subplots(2, 3, figsize=(18, 10), sharey=False)
-        
-        # Aplanar los ejes para iterar fácilmente
-        axes = axes.flatten()
-        
-        for idx, macronutriente in enumerate(macronutrientes):
-            sns.barplot(ax=axes[idx], x='Número de Cluster', y=macronutriente, data=promedios_grupos, palette='viridis')
-            
-            axes[idx].set_title(f'Promedio de {macronutriente} por Cluster')
-            axes[idx].set_xlabel('Número de Cluster')
-            axes[idx].set_ylabel(macronutriente)
-        
-        # Eliminar el último subgráfico si hay menos de 6
-        if len(macronutrientes) < len(axes):
-            fig.delaxes(axes[-1])
-        
-        # Ajustar el layout para evitar solapamientos
-        plt.tight_layout()
-        
-        
-        plt.show()
-
 graficos_cluster()
+
+# Proyectamos los alimentos de consumidores_libres (Y) en el subespacio del PCA anterior
+Y = filtrarAlimentos(consumidores_libres, tabla_nutricional)
+alimentos2 = Y['Alimento'].values
+Y = Y.iloc[:,1:].values
+proyector =  Vt_2
+Y_proyectado = proyectar(Y, proyector)
+
+# Graficamos la proyección con los clusters y obtenemos el DataFrame de clusters
+df_clusters2 = graficarProyeccion(Y_proyectado, alimentos2)
+graficos_cluster()
+
+
